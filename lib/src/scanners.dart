9e75dcb2c1fb931c7c489922e48e0dd025c9b281
luis <luisbigvargas@gmail.com>
2014-08-30 15:45:52 -0400

    Remove GetObjectsAnnotatedWith

--- lib/src/scanners.dart
+++ lib/src/scanners.dart
@@ -54,39 +54,6 @@ class IsAnnotation<T> {
   bool _isType(InstanceMirror am) => am.reflectee is T;
 }
 
-
-class GetObjectsAnnotatedWith<T> {
-
-  /**
-   * Scans currentMirrorSystem().isolate.rootLibrary and get the declarations that are
-   * [ClassMirror] and contains any annotation [T] and returns a List of reflectees
-   * of type [R]
-   *
-   *     //Get the list of Controllers
-   *     List<_Controller> controllers = new Scanner<_Controller, Object>().scan()
-   */
-  Iterable fromLibraries() {
-
-    var scannedObjects = [];
-
-    //For each library in currentSystem().libraries:
-    currentMirrorSystem().libraries.values.forEach(
-      //Add all reflectees in lm.declaration.values
-      (lm) => scannedObjects.addAll(lm.declarations.values
-        //Where:
-        .where((dm) =>
-          //declarationMirror is ClassMirror
-          dm is ClassMirror
-          //And declarationMirror.metadata contains any instanceMirror.reflectee with type T
-          && dm.metadata.any((im) => im.reflectee is T)
-        ).map(// map the result from where to a Reflectee
-              (dm) => (dm as ClassMirror).newInstance(const Symbol(''), []).reflectee)
-    ));
-
-    return scannedObjects;
-  }
-}
-
 /**
  * Get Declarations (methods and variables) annotated with [T] and are type [DM]
  */
