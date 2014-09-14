43c8195093762e790c27b6db4f78ae833467e5a3
luis <luisbigvargas@gmail.com>
2014-09-13 22:20:08 -0400

    Remove LoginController to use fast Mapping

--- example/controllers/authorization_sample_controller.dart
+++ example/controllers/authorization_sample_controller.dart
@@ -1,26 +1,17 @@
 part of drails_example;
 
-class LoginController {
-  static Map<int, User> users = {
-    1: new User()
-      ..id = 1
-      ..name = 'lulo'
-      ..password = 'lulo'
-      ..roles = ['ADMIN'],
-    2: new User()
-      ..id = 2
-      ..name = 'beto'
-      ..password = 'beto'
-      ..roles = ['PUBLIC']
-  };
-  
-  @Post
-  void login(HttpSession session, @RequestBody User user) {
-    var currentUser = users.values.singleWhere((u) => u.name == user.name && u.password == user.password);
-    session['user'] = currentUser; 
-  }
-  
-}
+Map<int, User> users = {
+  1: new User()
+    ..id = 1
+    ..name = 'lulo'
+    ..password = 'lulo'
+    ..roles = ['ADMIN'],
+  2: new User()
+    ..id = 2
+    ..name = 'beto'
+    ..password = 'beto'
+    ..roles = ['PUBLIC']
+};
 
 bool hasRolePublicOrAdmin(user, AuthorizeIf me) => 
     user.roles.any((role) => 
@@ -31,10 +22,12 @@ bool hasRolePublicOrAdmin(user, AuthorizeIf me) =>
 //@AuthorizeRoles(const ['PUBLIC', 'ADMIN'])
 class EmployeesController {
 
-  String get(int id) => 'employee: $id';
+  String get(int id) => 
+      'employee: $id';
   
   @AuthorizeRoles(const ['ADMIN'])
-  String save(int id, @RequestBody Map employee) => 'saved employee: $id, $employee';
+  String save(int id, @RequestBody Map employee) =>
+      'saved employee: $id, $employee';
   
   @DenyRoles(const ['PUBLIC'])
   Map saveAll(@RequestBody Map employee) => employee;
